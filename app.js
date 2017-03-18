6d4b929c5a24349baa50e15ec304e9bf13bf258c
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-18 16:49:35 +0200

    Animate flower head

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 TweenMax.to(".stem", 5, {visibility: "visible", width:"25%",
-            right:"45px", top:"320px", ease: Power0.easeNone});
+            right:"2px", top:"364px", ease: Power0.easeNone});
 
-TweenMax.to(".leaf1", 4, {right:"57px", top:"238px",
-      visibility: "visible", width: "10%", ease: Power1.easeOut}).delay(2);
+TweenMax.to(".leaf1", 4, {right:"16px", top:"287px",
+      visibility: "visible", width: "9%", ease: Power0.easeNone}).delay(1);
+
+TweenMax.to(".leaf3", 4, {top: "341px", left:"-117px", width: "7%",
+      visibility: "visible", ease: Power0.easeNone}).delay(1);
+
+TweenMax.to(".leaf2", 4, {top: "263px", right: "104px", width: "9%",
+    visibility: "visible", ease: Power0.easeNone}).delay(1);
+
+TweenMax.to(".flower", 3, {top: "260px", left: "45px", width: "35%",
+    visibility: "visible", ease: Power0.easeNone}).delay(2);
