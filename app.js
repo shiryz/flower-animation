7ae62b32b970f30b2725fd7f9167c8232a32a642
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-18 13:37:09 +0200

    Animate stem

---
+++ app.js
@@ -0,0 +1,5 @@
+TweenMax.to(".stem", 5, {visibility: "visible", width:"25%",
+            right:"45px", top:"320px", ease: Power0.easeNone});
+
+TweenMax.to(".leaf1", 4, {right:"57px", top:"238px",
+      visibility: "visible", width: "10%", ease: Power1.easeOut}).delay(2);
