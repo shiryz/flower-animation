58cbc3fb1bc13253ae6be75a417f13b9fc242882
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-18 17:50:24 +0200

    Replace tweenMax with timelineMax

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 // import { TweenMax, Power0 } from 'gsap';
 
-TweenMax.to(".stem", 5, {visibility: "visible", width:"25%",
+var t1 = new TimelineMax();
+
+t1.to(".stem", 5, {visibility: "visible", width:"25%",
             right:"2px", top:"364px", ease: Power0.easeNone});
 
-TweenMax.to(".leaf1", 4, {right:"16px", top:"287px",
-      visibility: "visible", width: "9%", ease: Power0.easeNone}).delay(1);
+t1.to(".leaf1", 4, {right:"16px", top:"287px",
+      visibility: "visible", width: "9%", ease: Power0.easeNone}, "-=4");
 
-TweenMax.to(".leaf3", 4, {top: "341px", left:"-117px", width: "7%",
-      visibility: "visible", ease: Power0.easeNone}).delay(1);
+t1.to(".leaf3", 4, {top: "341px", left:"-117px", width: "7%",
+      visibility: "visible", ease: Power0.easeNone}, "-=4");
 
-TweenMax.to(".leaf2", 4, {top: "263px", right: "104px", width: "9%",
-    visibility: "visible", ease: Power0.easeNone}).delay(1);
+t1.to(".leaf2", 4, {top: "263px", right: "104px", width: "9%",
+    visibility: "visible", ease: Power0.easeNone}, "-=4");
 
-TweenMax.to(".flower", 3, {top: "260px", left: "45px", width: "35%",
-    visibility: "visible", ease: Power0.easeNone}).delay(2);
+t1.to(".flower", 3, {top: "260px", left: "45px", width: "35%",
+    visibility: "visible", ease: Power0.easeNone}, "-=3");
